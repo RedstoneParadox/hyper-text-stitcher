2e1d6fed4c51bf73840ecb19ba57e9b8eddd3b52
RedstoneParadox <spinjitzu00@gmail.com>
2024-03-07 20:45:13 -0800

    Basic components
    Merging together HTML templates; no scripts yet

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
+mod process;
+
 use std::collections::HashMap;
 use std::fs;
-use std::path::Path;
-use html_editor::operation::Htmlifiable;
-use html_editor::parse;
+use std::path::{Components, Path};
+use html_editor::operation::{Editable, Htmlifiable, Queryable, Selector};
+use html_editor::{Node, parse};
+use html_editor::Node::{Element, Text};
 use serde::{Deserialize, Serialize};
+use crate::process::process_page;
+
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Config {
-    pub components: HashMap<String, ComponentConfig>
+    pub components: HashMap<String, ComponentConfig>,
+    pub pages: HashMap<String, PageConfig>
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct ComponentConfig {
+    pub path: String
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct PageConfig {
     pub path: String,
-    pub route: Option<String>
+    pub route: String
 }
 
 fn main() {
@@ -22,21 +33,40 @@ fn main() {
     let config_file = fs::read_to_string("components.yml").unwrap();
     let config: Config = serde_yaml::from_str(&*config_file).unwrap();
 
-    for pair in config.components {
+    for pair in config.pages {
         let name = pair.0;
-        let component_config = pair.1;
-        let document_path_string = format!("components{}/{}.html", component_config.path, name);
-        let document_path = Path::new(&*document_path_string);
-        let document_contents = fs::read_to_string(document_path).unwrap();
-        let mut dom = parse(&*document_contents).unwrap();
+        let page_config = pair.1;
+        let mut dom = load_document(&*name, &*page_config.path);
 
-        // Do script processing here
+        dom = process_page(&config.components, dom);
 
-        if let Some(route) = component_config.route {
-            let html = dom.html();
-            let page_path_string = format!("html{}/index.html", route);
-            let page_path = Path::new(&*page_path_string);
-            fs::write(page_path, &*html).unwrap();
-        }
+        let html = dom.html();
+        let page_path_string = format!("html{}/index.html", page_config.route);
+        let page_path = Path::new(&*page_path_string);
+        fs::write(page_path, &*html).unwrap();
     }
+}
+
+fn process(components: &HashMap<String, ComponentConfig>, dom: Vec<Node>) -> Vec<Node> {
+    let mut processed_dom = dom;
+
+    for component in components {
+        let name = component.0;
+        let element_name = format!("comp-{}", name);
+        let config = component.1;
+        let component_document = load_document(&*name, &*config.path);
+        let selector = Selector::from(&*element_name);
+        let processed_dom = processed_dom.replace_with(&selector, |it| {
+            Text("hello".to_string())
+        });
+    }
+
+    return processed_dom
+}
+
+fn load_document(name: &str, path: &str) -> Vec<Node> {
+    let document_path_string = format!("components{}/{}.html", path, name);
+    let document_path = Path::new(&*document_path_string);
+    let document_contents = fs::read_to_string(document_path).unwrap();
+    return parse(&*document_contents).unwrap();
 }
