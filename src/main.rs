ec387639f3833ff2d74e44f25d767a617bd9eaf1
RedstoneParadox <spinjitzu00@gmail.com>
2024-03-19 15:37:30 -0700

    Now allows for copy-pasting other directories

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
-
-
-use std::collections::HashMap;
 use std::fs;
-use std::path::{Path};
+use std::path::{Path, PathBuf};
 use serde::{Deserialize, Serialize};
 use tera::{Context, Tera};
 
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Config {
-    pub components: HashMap<String, ComponentConfig>,
-    pub pages: HashMap<String, PageConfig>
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct ComponentConfig {
-    pub path: String
+    pub include: Option<Vec<String>>,
+    pub pages: Vec<PageConfig>
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct PageConfig {
-    pub path: String,
-    pub route: String
+    pub template: String,
+    pub output: String
 }
-
-
 
 fn main() {
     let tera = match Tera::new("templates/**/*.html") {
@@ -34,14 +24,92 @@ fn main() {
             ::std::process::exit(1);
         }
     };
+    let config = load_config();
 
-    let page = match tera.render("home.html", &Context::new()) {
-        Ok(p) => p,
+    for page in config.pages {
+        println!("Rendering \"{}\"", page.output);
+        let rendered = match tera.render(&*page.template, &Context::new()) {
+            Ok(p) => p,
+            Err(e) => {
+                println!("Rendering error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        let output_dir = format!("html{}", page.output);
+
+        fs::write(&Path::new(&*output_dir), &*rendered).expect("TODO: panic message");
+    }
+
+    if let Some(dirs) = config.include {
+        for from in dirs {
+            println!("Copying directory \"{}\" to output directory", from);
+            let to = format!("html/{}", from);
+            copy(&Path::new(&*from), &Path::new(&*to)).expect("TODO: panic message");
+        }
+    }
+}
+
+fn load_config() -> Config {
+    let config_path = Path::new(&*"hypertext-stitcher.yml");
+    let config_file =  match fs::read_to_string(config_path) {
+        Ok(c) => c,
         Err(e) => {
-            println!("Rendering error(s): {}", e);
+            println!("Config error(s): {}", e);
             ::std::process::exit(1);
         }
     };
-    
-    fs::write(&Path::new(&*"html/index.html"), &*page).expect("TODO: panic message");
+    match serde_yaml::from_str::<Config>(&*config_file) {
+        Ok(c) => c,
+        Err(e) =>  {
+            println!("Config error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    }
+}
+
+pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::io::Error> {
+    let mut stack = Vec::new();
+    stack.push(PathBuf::from(from.as_ref()));
+
+    let output_root = PathBuf::from(to.as_ref());
+    let input_root = PathBuf::from(from.as_ref()).components().count();
+
+    while let Some(working_path) = stack.pop() {
+        println!("process: {:?}", &working_path);
+
+        // Generate a relative path
+        let src: PathBuf = working_path.components().skip(input_root).collect();
+
+        // Create a destination if missing
+        let dest = if src.components().count() == 0 {
+            output_root.clone()
+        } else {
+            output_root.join(&src)
+        };
+        if fs::metadata(&dest).is_err() {
+            println!(" mkdir: {:?}", dest);
+            fs::create_dir_all(&dest)?;
+        }
+
+        for entry in fs::read_dir(working_path)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                stack.push(path);
+            } else {
+                match path.file_name() {
+                    Some(filename) => {
+                        let dest_path = dest.join(filename);
+                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
+                        fs::copy(&path, &dest_path)?;
+                    }
+                    None => {
+                        println!("failed: {:?}", path);
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
