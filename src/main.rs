6a9d4db8c2b130ea2dfe26c7e7dcf6ec15d43bfb
RedstoneParadox <spinjitzu00@gmail.com>
2024-03-19 16:12:21 -0700

    Remove unnecessary printing

--- src/main.rs
+++ src/main.rs
@@ -75,8 +75,6 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
     let input_root = PathBuf::from(from.as_ref()).components().count();
 
     while let Some(working_path) = stack.pop() {
-        println!("process: {:?}", &working_path);
-
         // Generate a relative path
         let src: PathBuf = working_path.components().skip(input_root).collect();
 
@@ -87,7 +85,6 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
             output_root.join(&src)
         };
         if fs::metadata(&dest).is_err() {
-            println!(" mkdir: {:?}", dest);
             fs::create_dir_all(&dest)?;
         }
 
@@ -100,11 +97,9 @@ pub fn copy<U: AsRef<Path>, V: AsRef<Path>>(from: U, to: V) -> Result<(), std::i
                 match path.file_name() {
                     Some(filename) => {
                         let dest_path = dest.join(filename);
-                        println!("  copy: {:?} -> {:?}", &path, &dest_path);
                         fs::copy(&path, &dest_path)?;
                     }
                     None => {
-                        println!("failed: {:?}", path);
                     }
                 }
             }
